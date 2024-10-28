798782e26e4839b182b9cec0beed32474fafc417
hosainahmed <hosainahmed534745@gmail.com>
2024-10-28 21:07:01 +0600

    finally solve the problems

--- src/Components/Hook/useAxiosPublic.jsx
+++ src/Components/Hook/useAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 function useAxiosPublic() {
   const axiosPublic = axios.create({
-    // baseURL: "https://caff-canvas.vercel.app",
-    baseURL: "http://localhost:5000",
+    baseURL: "https://caff-canvas.vercel.app",
+    // baseURL: "http://localhost:5000",
   });
 
   return axiosPublic;
